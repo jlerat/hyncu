cafcb2d41e049f05c525524ca38dcc493d6faa13
Julien Lerat <julien.lerat@csiro.au>
2024-11-14 11:39:32 +1100

    feat: added storage of stations info

--- hyncu/nc4dataframe.py
+++ hyncu/nc4dataframe.py
@@ -8,23 +8,56 @@ import netCDF4
 
 from hyncu import nc4io
 
-DEFAULT_DATANAME = "site_timeseries_data"
+DEFAULT_DATANAME = "data"
+
+def validate_units(units):
+    for u in units:
+        try:
+            cf_units.Unit(u)
+        except:
+            errmess = f"Unit {u} is not valid."
+            raise ValueError(errmess)
 
 
-def configure_netcdf_file(nc4dset, siteids, variables, times,\
+def get_item_index(nc4dset, item_type, item_name):
+    assert item_type in ["stationid", "variable"]
+    dim = nc4dset[item_type][:]
+    return np.where(item_name==dim)[0][0]
+
+
+def get_colnames(nc4dset, vname):
+    varnames = nc4dset[vname][:]
+    units = nc4dset[f"{vname}_units"][:]
+    vtxtdim = "station_info_txt_variable"
+    if vtxtdim in nc4dset.dimensions:
+        vtxtnames = nc4dset[vtxtdim][:]
+    else:
+        vtxtnames = []
+
+    colnames = []
+    for v, u in zip(varnames, units):
+        cn = f"{v}[{u}]" if not v in vtxtnames else v
+        colnames.append(cn)
+
+    return colnames
+
+
+
+def configure_netcdf_file(nc4dset, stationids, variables, times,\
                     units=None, dataname=DEFAULT_DATANAME, attrs=None):
     # Create dimensions
-    nc4io.add_dimensions(nc4dset, time=times, siteid=siteids, variable=variables)
+    kw = {f"{dataname}_variable": variables}
+    nc4io.add_dimensions(nc4dset, time=times, stationid=stationids, **kw)
 
     # Variable to store data
-    var = nc4io.Variable(dataname, ["siteid", "time", "variable"], \
+    var = nc4io.Variable(dataname, ["stationid", "time", f"{dataname}_variable"], \
                                 units="-", \
                                 chunksizes=(1, len(times),len(variables)), \
                                 attrs=attrs)
     var.to_dataset(nc4dset)
 
     # Units
-    un = nc4io.Variable("variable_units", ["variable"], units="-", \
+    un = nc4io.Variable(f"{dataname}_variable_units", [f"{dataname}_variable"], units="-", \
                         dtype=str, fill_value="NA", \
                         significant_digit=None, \
                         compression=None, \
@@ -34,45 +67,103 @@ def configure_netcdf_file(nc4dset, siteids, variables, times,\
         units = ["-"]*len(variables)
 
     units = np.array(units)
+    validate_units(units)
     assert len(units) == len(variables)
-
-    for u in units:
-        try:
-            cf_units.Unit(u)
-        except:
-            errmess = f"Unit {u} is not valid."
-            raise ValueError(errmess)
-
-    nc4dset["variable_units"][:] = units
+    nc4dset[f"{dataname}_variable_units"][:] = units
 
     # Meta data
     nc4io.add_meta(nc4dset)
 
 
-def get_item_index(nc4dset, item_type, item_name):
-    assert item_type in ["siteid", "variable"]
-    dim = nc4dset[item_type][:]
-    return np.where(item_name==dim)[0][0]
+def set_station_info(nc4dset, stations):
+    # Split stations into num and txt
+    stations_num = stations.select_dtypes(include="number")
+    stations_txt = stations.select_dtypes(include=["object", "datetime"]).astype(str)
+
+    assert "NAME" in stations_txt.columns
+
+    # Set station id dim if not existing
+    stationids = np.array(stations.index.values).astype(str)
+    if not "stationid" in nc4dset.dimensions:
+        dim = nc4io.Dimension("stationid", stationids, stationids.dtype, "-")
+        dim.to_dataset(nc4dset)
+
+    else:
+        assert np.all(stationids == nc4dset["stationid"][:])
+
+    for itype, info in dict(num=stations_num, txt=stations_txt).items():
+        # Get station info variables and units
+        variables = info.columns.str.replace("\\[.*", "", regex=True).values.astype(str)
+        if itype == "num":
+            units = info.columns.str.replace(".*\\[|\\]$", "", regex=True).values.astype(str)
+            units[units==variables] = "-"
+        else:
+            units = np.array(["-"]*len(variables))
+
+        # Create dimensions
+        vname = f"station_info_{itype}_variable"
+        vdim = nc4io.Dimension(vname, variables, variables.dtype, "-")
+        vdim.to_dataset(nc4dset)
+
+        # Variable to store stations info
+        dname = f"station_info_{itype}"
+        if itype == "num":
+            values = info.values
+            fill_value = np.nan
+            digit = 7
+        else:
+            values = info.values.astype(str)
+            fill_value = "NA"
+            digit = None
+
+        dtype = values.dtype
+        attrs = dict(description="Station info / {itype}")
+        var = nc4io.Variable(dname, ["stationid", vname], \
+                                    units="-", dtype=dtype, \
+                                    fill_value=fill_value, \
+                                    significant_digit=digit, \
+                                    chunksizes=values.shape, \
+                                    compression=None, \
+                                    attrs=attrs)
+        var.to_dataset(nc4dset)
+        nc4dset[dname][:] = values
+
+        # Units
+        uname = f"station_info_{itype}_variable_units"
+        un = nc4io.Variable(uname, [vname], units="-", \
+                            dtype=units.dtype, fill_value="NA", \
+                            significant_digit=None, \
+                            compression=None, \
+                            attrs={"description": "Units for each station info variable"})
+        un.to_dataset(nc4dset)
+        units = np.array(units)
+        validate_units(units)
+        assert len(units) == len(variables)
+        nc4dset[uname][:] = units
 
 
-def get_colnames(nc4dset):
-    varnames = nc4dset["variable"][:]
-    units = nc4dset["variable_units"][:]
-    return [f"{v}[{u}]" for v, u in zip(varnames, units)]
+def get_station_info(nc4dset):
+    df = []
+    stationids = nc4dset["stationid"][:]
+    for itype in ["num", "txt"]:
+        info = nc4dset[f"station_info_{itype}"][:]
+        colnames = get_colnames(nc4dset, f"station_info_{itype}_variable")
+        df.append(pd.DataFrame(info, index=stationids, columns=colnames))
+    return pd.concat(df, axis=1)
 
 
-def set_dataframe(nc4dset, siteid, df, dataname=DEFAULT_DATANAME):
-    isite = get_item_index(nc4dset, "siteid", siteid)
-    colnames = get_colnames(nc4dset)
+def set_dataframe(nc4dset, stationid, df, dataname=DEFAULT_DATANAME):
+    istation = get_item_index(nc4dset, "stationid", stationid)
+    colnames = get_colnames(nc4dset, f"{dataname}_variable")
     # TODO
-    nc4dset[dataname][isite, :, :] = data
+    nc4dset[dataname][istation, :, :] = data
 
 
-def get_dataframe(nc4dset, siteid, dataname=DEFAULT_DATANAME):
-    isite = get_item_index(nc4dset, "siteid", siteid)
-    colnames = get_colnames(nc4dset)
+def get_dataframe(nc4dset, stationid, dataname=DEFAULT_DATANAME):
+    istation = get_item_index(nc4dset, "stationid", stationid)
+    colnames = get_colnames(nc4dset, f"{dataname}_variable")
     tdim = nc4io.TimeDimension.from_dataset(nc4dset)
     times = tdim.values
-    df = pd.DataFrame(nc4dset[dataname][isite, :, :], index=times, \
+    df = pd.DataFrame(nc4dset[dataname][istation, :, :], index=times, \
                             columns=colnames)
     return df
--- tests/test_nc4dataframe.py
+++ tests/test_nc4dataframe.py
@@ -2,6 +2,7 @@ import math
 from pathlib import Path
 
 import pytest
+from string import ascii_letters as letters
 
 import numpy as np
 np.random.seed(5446)
@@ -18,23 +19,51 @@ FHERE = Path(__file__).resolve().parent
 def test_dataframe(allclose):
     fnc = FHERE / "test_dataframe.nc"
     with Dataset(fnc, "w") as nc:
-        sites = ["a", "b", "c"]
+        stations = ["a", "b", "c"]
         variables = ["v1", "v2", "v3", "v4"]
         times = pd.date_range("1990-01-01", "2000-12-31", freq="D")
         dataname = "truc"
         units = ["mm.day-1", "m3.s-1", "degC", "kg"]
-        nc4dataframe.configure_netcdf_file(nc, siteids=sites, \
+        nc4dataframe.configure_netcdf_file(nc, stationids=stations, \
                     variables=variables, \
                     units=units, \
                     times=times, \
                     dataname=dataname)
 
-        size = (len(sites), len(times), len(variables))
+        assert "time" in nc.dimensions
+        assert "stationid" in nc.dimensions
+        assert f"{dataname}_variable" in nc.dimensions
+        assert f"{dataname}_variable_units" in nc.variables
+        assert dataname in nc.variables
+
+        size = (len(stations), len(times), len(variables))
         nc[dataname][:] = np.random.uniform(-1, 1, size=size)
 
     with Dataset(fnc, "r") as nc:
         df = nc4dataframe.get_dataframe(nc, "a", dataname)
         assert df.shape == (len(times), len(variables))
+
+    fnc.unlink()
+
+
+def test_stations(allclose):
+    fnc = FHERE / "test_dataframe_stations.nc"
+    with Dataset(fnc, "w") as nc:
+        stations = ["a", "b", "c"]
+        variables = ["v1", "v2[m/s]", "v3[-]", "v4"]
+
+        # Random station info
+        d = np.random.uniform(0, 1, (len(stations), len(variables)))
+        df = pd.DataFrame(d, index=stations, columns=variables)
+        df.loc[:, "NAME"] = ["".join([letters[i] for i in np.random.randint(0, 26, 10)]) for s in stations]
+        df.loc[:, "TRUC[-]"] = ["".join([letters[i] for i in np.random.randint(0, 26, 10)]) for s in stations]
+
+        nc4dataframe.set_station_info(nc, df)
+
+    with Dataset(fnc, "r") as nc:
+        df2 = nc4dataframe.get_station_info(nc)
+        assert df2.shape == df.shape
+        assert allclose(df2.iloc[:, :4].values, df.iloc[:, :4].values, atol=1e-6)
 
     fnc.unlink()
 
