ffd4fc501542a8b5e3bccc7013647608722fee3c
Julien Lerat <julien.lerat@csiro.au>
2024-12-06 13:29:56 +1100

    test: fixed df test

--- tests/test_nc4dataframe.py
+++ tests/test_nc4dataframe.py
@@ -40,25 +40,11 @@ def test_dataframe(allclose):
         size = (len(times), len(variables))
         df = pd.DataFrame(np.random.uniform(-1, 1, size=size), \
                                 index=times, columns=variables)
-        nc4dataframe.set_dataframe(nc, "a", df, dataname)
-
-
-        size = (len(times)-10, len(variables)-2)
-        df2 = pd.DataFrame(np.random.uniform(-1, 1, size=size), \
-                index=times[:-10], columns=variables[:-2])
-        nc4dataframe.set_dataframe(nc, "b", df2, dataname)
-
+        nc4dataframe.set_data(nc, "a", df, dataname)
 
     with Dataset(fnc, "r") as nc:
         df = nc4dataframe.get_data(nc, "a", dataname)
         assert df.shape == (len(times), len(variables))
-
-        df2 = nc4dataframe.get_dataframe(nc, "b", dataname, False)
-        assert df2.shape == (len(times), len(variables))
-        assert df2.iloc[:, 2:].isnull().all().all()
-
-        df2 = nc4dataframe.get_dataframe(nc, "b", dataname)
-        assert df2.shape == (len(times)-10, len(variables))
 
     fnc.unlink()
 
